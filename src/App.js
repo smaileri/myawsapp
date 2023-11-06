2b472ce893e39c610dbd3bc41f1eefcb41352b88
Viktor Melnikov <Victor.Melnikov96@gmail.com>
2023-11-06 12:04:40 +0100

    ver3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          V2
+          V3
         </p>
         <a
           className="App-link"
